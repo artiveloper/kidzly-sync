d09c3a6cbaf5a59b791c482e118ce17b35130839
artiveloper <artiveloper@gmail.com>
2026-04-17 21:37:37 +0900

    refactor: 사용하지 않는 파일 제거

--- src/main/kotlin/kr/kidzly/sync/KidzlySyncApplication.kt
+++ src/main/kotlin/kr/kidzly/sync/KidzlySyncApplication.kt
@@ -4,10 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
-import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
-@EnableScheduling
 @EnableRetry
 @ConfigurationPropertiesScan
 class KidzlySyncApplication
